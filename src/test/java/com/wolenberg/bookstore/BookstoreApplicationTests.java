ed879075cc637ed81aec0b3ccce552879e9a3381
wolenberg <wolembergjp@gmail.com>
2021-05-22 16:37:46 -0300

    Commit inicial

---
+++ src/test/java/com/wolenberg/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolenberg.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
