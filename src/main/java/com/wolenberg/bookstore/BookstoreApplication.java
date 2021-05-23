2e1773bf330d8ad3b54f500a73723fbee906dba3
wolenberg <wolembergjp@gmail.com>
2021-05-23 09:25:13 -0300

    Instacia de forma automática da base da dados

--- src/main/java/com/wolenberg/bookstore/BookstoreApplication.java
+++ src/main/java/com/wolenberg/bookstore/BookstoreApplication.java
@@ -1,13 +1,41 @@
 package com.wolenberg.bookstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.wolenberg.bookstore.domain.Categoria;
+import com.wolenberg.bookstore.domain.Livro;
+import com.wolenberg.bookstore.repositories.CategoriaRepository;
+import com.wolenberg.bookstore.repositories.LivroRepository;
+
 @SpringBootApplication
-public class BookstoreApplication {
+public class BookstoreApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private LivroRepository livroRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática", "Livros de T.I");
+
+		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem ipsum", cat1);
+
+		cat1.getLivros().addAll(Arrays.asList(l1));
+
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+
 	}
 
 }
