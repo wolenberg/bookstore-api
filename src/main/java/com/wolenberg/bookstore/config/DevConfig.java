980787d6a97c902d04dc69323c8f1a8877eae3e9
wolenberg <wolembergjp@gmail.com>
2021-05-23 10:29:28 -0300

    Criação do Perfil de Dev

---
+++ src/main/java/com/wolenberg/bookstore/config/DevConfig.java
@@ -0,0 +1,29 @@
+package com.wolenberg.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.wolenberg.bookstore.service.DbService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DbService dbService;
+
+	@Value("$(spring.jpa.hibernate.ddl-auto)")
+	private String strategy;
+
+	@Bean
+	public boolean instanciaBaseDeDados() {
+
+		if (strategy.equals("create")) {
+			this.dbService.instanciaBaseDeDados();
+		}
+		return false;
+	}
+}
