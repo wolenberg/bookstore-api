c5be2d2d508ac1a9a3ffcc00973bbfa981c042d5
wolenberg <wolembergjp@gmail.com>
2021-05-23 22:10:53 -0300

    Post para categoria

--- src/main/java/com/wolenberg/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/wolenberg/bookstore/resources/CategoriaResource.java
@@ -1,15 +1,18 @@
 package com.wolenberg.bookstore.resources;
 
+import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.wolenberg.bookstore.domain.Categoria;
 import com.wolenberg.bookstore.dtos.CategoriaDTO;
@@ -35,6 +38,14 @@ public class CategoriaResource {
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj) {
+		obj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+
 	}
 }
 
