109219496763d33e15807c87ddc44f9da72e3ac3
wolenberg <wolembergjp@gmail.com>
2021-05-25 20:01:22 -0300

    Delete para categoria

--- src/main/java/com/wolenberg/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/wolenberg/bookstore/resources/CategoriaResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,6 +55,13 @@ public class CategoriaResource {
 		Categoria newObj = service.update(id, objDto);
 		return ResponseEntity.ok().body(new CategoriaDTO(newObj));
 
+	}
+	
+	@DeleteMapping(value = ("/{id}"))
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+		
 	}
 
 }
--- src/main/java/com/wolenberg/bookstore/service/CategoriaService.java
+++ src/main/java/com/wolenberg/bookstore/service/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.wolenberg.bookstore.domain.Categoria;
@@ -24,21 +25,27 @@ public class CategoriaService {
 				"Objeto não encontrado! Id:" + id + " Tipo: " + Categoria.class.getName()));
 
 	}
-	
+
 	public List<Categoria> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
 	}
-	
+
 	public Categoria update(Integer id, CategoriaDTO objDto) {
 		Categoria obj = findbyId(id);
 		obj.setNome(objDto.getNome());
 		obj.setDescricao(objDto.getDescricao());
 		return repository.save(obj);
 	}
-	
+
+	public void delete(Integer id) {
+		findbyId(id);
+		repository.deleteById(id);
+		
+	}
+
 }
